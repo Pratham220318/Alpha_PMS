c156d7f2728da63ef8162d5058cd5ffe8663ba1e
Pratham220318 <prathameshethiraj24@gmail.com>
2026-05-24 18:05:02 +0530

    connected railway database

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,24 @@
+// import mysql from "mysql2/promise";
+
+// const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "Pratham@24",
+//     database: "alpha_pms_db",
+//     waitForConnections: true,
+//     connectionLimit: 100,
+// });
+
+// export default db;
+
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Pratham@24",
-    database: "alpha_pms_db",
-    waitForConnections: true,
-    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 export default db;
